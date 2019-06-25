7fb2f10fb1caed180aaedf01c5a486955ff798a6
“giandoso” <a15029@bcc.unifal-mg.edu.br>
2019-06-25 17:52:45 -0300

    PDA

--- src/PDA/Main.java
+++ src/PDA/Main.java
@@ -16,7 +16,11 @@ public class Main {
     static boolean[] palavras_aceitas;
 
     public static void main(String[] args) throws FileNotFoundException {
-        Reader r = new Reader("src/PDA/automato2.input");
+        Reader r = new Reader("src/PDA/automato3.input");
+        // Automato 1 = (a^n)(b^n) | n>0
+        // Automato 2 = Quantidade igual de a e b
+        // Automato 3 = Palindromo
+        // Automato 4 = (b^n)(a^n) | n>0
         Automato a = new Automato();
         a.set_estados(r.get_estados());
 
@@ -30,7 +34,7 @@ public class Main {
         }
         for (int i = 0; i < Reader.palavras.size(); i++) {
             String palavra = Reader.palavras.get(i);
-            a.verifica_cadeia(palavra, palavra, estado_inicial, r.get_inicio_da_pilha(), r.get_inicio_da_pilha(), false, i);
+            a.verifica_cadeia(palavra, palavra, estado_inicial, r.get_inicio_da_pilha(), r.get_inicio_da_pilha(), true, i);
         }
         for (int i = 0; i < Reader.palavras.size(); i++) {
             System.out.print("Palavra: " + Reader.palavras.get(i) + "\t\t\t| ");
